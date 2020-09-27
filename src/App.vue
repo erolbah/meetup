8b10464cbce3efed61ba1a84e0711edf83928e55
Erol Bakir <erol@2jours.nl>
2020-09-27 11:52:45 +0200

    router && routerlink in navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,68 @@
 <template>
   <v-app>
-    <v-navigation-drawer fixed v-model="sideNav">
-      <v-list disabled>
+    <!-- <v-navigation-drawer fixed v-model="sideNav">
+      <v-list nav disabled>
         <v-subheader>MENU</v-subheader>
         <v-list-item-group color="primary">
-          <v-list-item>
+          <v-list-item v-for="item in menuItems" :key="item.title" link>
             <v-list-item-icon>
-              <v-icon>mdi-account-multiple</v-icon>
+              <v-icon>{{item.icon}}</v-icon>
             </v-list-item-icon>
             <v-list-item-content>
-              <v-list-item-title>View Meetups</v-list-item-title>
+              <v-list-item-title>{{item.title}}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
-    </v-navigation-drawer>
+    </v-navigation-drawer> -->
+
+    <v-card>
+      <v-navigation-drawer fixed v-model="sideNav">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              MENU
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense nav>
+          <v-list-item
+            v-for="item in menuItems"
+            :key="item.title"
+            :to="item.link" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+
     <v-card>
       <v-toolbar>
         <v-app-bar-nav-icon
           class="hidden-sm-and-up"
           @click="sideNav = !sideNav"></v-app-bar-nav-icon>
-        <v-toolbar-title>Meetup</v-toolbar-title>
+        <v-toolbar-title>
+          <router-link to="/" tag="span" style="cursor:pointer">Meetup</router-link> 
+        </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-xs-only">
-          <v-btn text>
-            <v-icon left>mdi-account-multiple</v-icon> View Meetups
+        <v-toolbar-items v-for="item in menuItems" :key="item.title" class="hidden-xs-only">
+          <v-btn :to="item.link" link text>
+            <v-icon left>{{item.icon}}</v-icon> {{item.title}}
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
     </v-card>
     <main>
-
+      <router-view></router-view>
     </main>
   </v-app>
 </template>
@@ -46,7 +78,14 @@ export default {
 
   data () {
     return {
-      sideNav: false
+      sideNav: false,
+      menuItems: [
+        {link: '/meetups', icon: 'mdi-account-multiple', title: 'View Meetups'},
+        {link: '/meetup/new', icon: 'mdi-map-marker', title: 'Orginaze Meetup'},
+        {link: '/profile', icon: 'mdi-account', title: 'Profile'},
+        {link: '/signup', icon: 'mdi-face', title: 'Sign Up'},
+        {link: '/signin', icon: 'mdi-lock-open', title: 'Sign In'}
+      ]
     }
   },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/components/Home.vue'
+import Meetups from '@/components/Meetup/Meetups.vue'
+import CreateMeetup from '@/components/Meetup/CreateMeetup.vue'
+import Profile from '@/components/User/Profile.vue'
+import Signin from '@/components/User/Signin.vue'
+import Signup from '@/components/User/Signup.vue'
 
 Vue.use(VueRouter)
 
@@ -11,16 +16,34 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/meetups',
+    name: 'Meetups',
+    component: Meetups
+  },
+  {
+    path: '/meetup/new',
+    name: 'CreateMeetup',
+    component: CreateMeetup
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin
+  },
+  {
+    path: '/Signup',
+    name: 'Signup',
+    component: Signup
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
