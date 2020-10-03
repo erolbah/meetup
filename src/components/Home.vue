394ff4dd00cdf360129179dca8bf83d85bce1118
Erol Bakir <erol@2jours.nl>
2020-10-03 20:31:10 +0200

    carousel done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,63 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col align="center">
+    <v-row xs12>
+      <v-col align="center" xs12 sm6 class="text-sm-right text-xs-center">
         <v-btn class="primary" large center to="/meetups">Explore Meetups</v-btn>
       </v-col>
-      <v-col align="center">
+      <v-col align="center" xs12 sm6 class="text-sm-left text-xs-center">
         <v-btn class="primary" large to="/meetups/new">Orginaze Meetup</v-btn>
       </v-col>
-      
+    </v-row>
+    <v-row>
+      <v-carousel
+        cycle
+        height="400"
+        hide-delimiter-background
+        show-arrows-on-hover>
+        <v-carousel-item
+          v-for="(slide, i) in slides"
+          :key="i">
+          <v-sheet
+            :color="colors[i]"
+            height="100%">
+            <v-row
+              class="fill-height"
+              align="center"
+              justify="center">
+              <div class="display-3">{{ slide }} Slide</div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+    </v-row>
+    <v-row xs12>
+      <v-col align="center" class="text-xs-center">
+        <p>Join our Meetups!</p>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-
+data () {
+return {
+  colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: [
+        'First',
+        'Second',
+        'Third',
+        'Fourth',
+        'Fifth',
+      ],
+    } 
+  }
 }
 </script>
 
