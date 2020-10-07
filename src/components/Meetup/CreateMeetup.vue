094bf981dd1ebcbf8d9af347c84cf177d1eb3051
Erol Bakir <erol@2jours.nl>
2020-10-07 21:29:10 +0200

    meetup page start

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,48 @@
 <template>
-  <div><p>Create Meetups</p></div>
+  <v-container>
+    <v-row xs12>
+      <v-col xs12>
+        <v-card
+            :color="item.color"
+            dark>
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="headline"
+                  v-text="item.title"
+                ></v-card-title>
+                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+                <v-card-action>
+                  <v-btn
+                    v-if="item.artist === 'Ellie Goulding'"
+                    class="ml-2 mt-3"
+                    fab
+                    icon
+                    height="40px"
+                    right
+                    width="40px"><v-icon>mdi-play</v-icon>
+                  </v-btn>
+                  <v-btn
+                    v-else
+                    class="ml-2 mt-5"
+                    outlined
+                    rounded
+                    small>
+                    START RADIO</v-btn>
+                </v-card-action>
+              </div>
+
+              <v-avatar
+                class="ma-3"
+                size="125"
+                tile>
+                <v-img :src="item.src"></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,10 +1,64 @@
 <template>
-  <div><p>The Meetups</p></div>
+  <v-container>
+    <v-row xs12>
+      <v-col xs5 v-for="(item, i) in items"
+          :key="i"
+          >
+        <v-card
+            :color="item.color"
+            dark>
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="headline"
+                  v-text="item.title"
+                ></v-card-title>
+                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+                <v-card-action>
+                  <v-btn
+                    class="ml-2 mt-3"
+                    fab
+                    icon
+                    height="40px"
+                    right
+                    width="40px"><v-icon>mdi-play</v-icon>
+                  </v-btn>
+                </v-card-action>
+              </div>
+
+              <v-avatar
+                class="ma-3"
+                size="125"
+                tile>
+                <v-img :src="item.src"></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+data: function() {
+return {
+    items: [
+        {
+          color: '#1F7087',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+        },
+        {
+          color: '#952175',
+          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+          title: 'Halcyon Days',
+          artist: 'Ellie Goulding',
+        },
+      ],
+  }
+}
 }
 </script>
 
