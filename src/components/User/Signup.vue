6a91e9cc54a02962215cd3b2f8121a5aa80fc45a
Erol Bakir <erol@2jours.nl>
2020-11-22 22:16:14 +0100

    firebase added && login temp done

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,10 +1,74 @@
 <template>
-  <p>Sign up</p>
+  <v-container>
+    <v-row>
+      <v-col xs="12">
+        <form @submit.prevent="onCreateMeetup">
+          <v-row>
+            <v-col xs12 sm="6" offset-sm="3">
+              <v-text-field
+                name="email"
+                label="Mail"
+                id="email"
+                v-model="email"
+                type="email"
+                required>
+
+              </v-text-field>
+            </v-col>
+            <v-col xs12 sm="6" offset-sm="3">
+              <v-text-field
+                name="password"
+                label="Password"
+                id="password"
+                v-model="password"
+                type="password"
+                required>
+
+              </v-text-field>
+            </v-col>
+             <v-col xs12 sm="6" offset-sm="3">
+              <v-text-field
+                name="confirmPassword"
+                label="Confirm Password"
+                id="confirmPassword"
+                v-model="confirmPassword"
+                type="password"
+                :rules="[comparePasswords]">
+                
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col xs="12" sm="6" offset-sm="3">
+              <v-btn class="primary"
+                    type="submit">Sign Up</v-btn>
+            </v-col>
+          </v-row>
+        </form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    comparePasswords () {
+      return this.password !== this.confirmPassword ? 'Passwords do not match' : true
+    }
+  },
+  data () {
+    return {
+      email: '',
+      password:'',
+      confirmPassword: ''
+    }
+  },
+  methods: {
+    onSignUp () {
+      console.log(this.email, this.password, this.confirmPassword)
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import DateFormat from './filter/date';
+import * as firebase from 'firebase';
 
 Vue.config.productionTip = false
 // Vue.config.globalProperties.$filters = {
@@ -18,5 +19,17 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCvJ0D6aTOldEtODzjsI2L2HB6W8Esbuiw",
+      authDomain: "sportware-a9316.firebaseapp.com",
+      databaseURL: "https://sportware-a9316.firebaseio.com",
+      projectId: "sportware-a9316",
+      storageBucket: "sportware-a9316.appspot.com",
+      messagingSenderId: "140450372211",
+      appId: "1:140450372211:web:f6b58b7a465cf02412a806",
+      measurementId: "G-2EWF0LD6PT"
+    })
+  }
 }).$mount('#app')
