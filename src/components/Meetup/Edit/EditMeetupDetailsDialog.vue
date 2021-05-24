e62d7f303744e9b5a7b17309c464767dc74b9221
erolbah <erolbakir@gmail.com>
2021-05-24 22:16:34 +0200

    slot problems

--- src/components/Meetup/Edit/EditMeetupDetailsDialog.vue
+++ src/components/Meetup/Edit/EditMeetupDetailsDialog.vue
@@ -5,5 +5,16 @@
         mdi-pencil
       </v-icon>
     </v-btn>
+    <v-card>
+      <v-container>
+        <v-row xs12>
+          <v-card-title>Edit Meetup</v-card-title>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row xs12>
+          content
+        </v-row>
+      </v-container>
+    </v-card>
   </v-dialog>
 </template>
